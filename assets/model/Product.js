4b098808cb5a703de85441b441c9f0310ca7921b
HuyVN <buiiduchuy@gmail.com>
2024-07-12 10:27:53 +0700

    add code

---
+++ assets/model/Product.js
@@ -0,0 +1,12 @@
+// khởi tạo 1 lớp đối tượng Product cho bài tập shop bán hàng
+
+class Product {
+  name= "";
+  price= "";
+  screen="";
+  backCamera="";
+  frontCamera="";
+  img="";
+  desc="";
+  type="";
+}
